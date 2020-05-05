d9b051ba4ffeabec7bbcb34c26efef4f3db60661
kishan sharma <kishu02@hotmail.com>
2020-05-05 21:32:46 +0545

    tutorial for ode2Food udemy

---
+++ IGreeter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ode2Food
+{
+    public interface IGreeter
+    {
+        string greetingsOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _confoguration;
+        public Greeter(IConfiguration configuration)
+        {
+            _confoguration = configuration;
+        }
+
+        public string greetingsOfTheDay()
+        {
+            if (!string.IsNullOrEmpty(_confoguration["greeting"]))
+                return _confoguration["greeting"];
+            else
+                return "Greetings fellow developers";
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ode2Food
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+                       
+            services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IGreeter greeter, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
+
+            app.UseFileServer();
+            app.UseMvcWithDefaultRoute();
+
+            //app.UseRouting();
+            app.Use(next =>
+           {
+               return async context =>
+               {
+                   logger.LogInformation("Incomming Request");
+                   if (context.Request.Path.StartsWithSegments("/mym"))
+                   {
+                       await context.Response.WriteAsync("middleware hit");
+                       logger.LogInformation("Handeled");
+
+                   }
+                   else
+                   {
+                       await next(context);
+                       logger.LogInformation("Response out going");
+
+                   }
+
+               };
+           });
+
+            app.UseWelcomePage(new WelcomePageOptions
+            {
+                Path = "/welcome"
+            });
+
+            app.Run(async context =>
+               {
+                   //throw new Exception("Error");
+                   var greeting = $"{greeter.greetingsOfTheDay()} in {env.EnvironmentName}";
+                   await context.Response.WriteAsync(greeting);
+               });
+        }
+    }
+}
