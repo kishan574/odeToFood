f81f0ddf7c30522b78fea1a13eb02958dd259a2c
kishan sharma <kishu02@hotmail.com>
2020-05-23 21:43:42 +0545

    Added Data Anotations to the Add Resturant Form Module.

--- Models/Resturant.cs
+++ Models/Resturant.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ode2Food.Models
 {
     public class Resturant
     {
         public int Id { get; set; }
+        [Display(Name = "Resturant Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Cusine")]
         public Cusine CusineType { get; set; }
     }
 }
