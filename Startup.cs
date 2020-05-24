564a892e85a7b1702665ce5d11cd9748fb52467c
kishan sharma <kishu02@hotmail.com>
2020-05-24 19:49:45 +0545

    Configuring entity framework and setting up CLI tool for ef 6

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,6 @@ namespace ode2Food
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddSingleton<IGreeter, Greeter>();
             services.AddSingleton<IResturant, InMemoryResturant>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
