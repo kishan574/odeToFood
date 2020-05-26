c7c61d04581f80de28b89293b289a8ddb0510424
kishan sharma <kishu02@hotmail.com>
2020-05-26 20:06:48 +0545

    added a flag and a cmnt for future refrence about the design pattern to followed as per the ef core.

--- DataConfiguration/OdeToFoodDbContext.cs
+++ DataConfiguration/OdeToFoodDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using ode2Food.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,14 @@ namespace ode2Food.DataConfiguration
 {
     public class OdeToFoodDbContext : DbContext
     {
+        //Design pattern must be strictly followed!! 
+        //as {fileName}DbContext.
+        //else Following exception occurs as the entity framework core 
+        //follows the strictly followed design pattern.
+        //Exception --> 
+        //Unable to create an object of type 'DataContext'. 
+        //For the different patterns supported at design time, 
+        //see https://go.microsoft.com/fwlink/?linkid=851728
         public OdeToFoodDbContext(DbContextOptions options) : base(options)
         {
             
