fc4d9076a5d8773b7ba1c1efc2283ebef06e54eb
kishan sharma <kishu02@hotmail.com>
2020-05-30 12:44:42 +0545

    passing an IService to a razor page using code_behind page model

--- Pages/Greeting.cshtml.cs
+++ Pages/Greeting.cshtml.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ode2Food.Services;
 
 namespace ode2Food.Pages
 {
-    public class IndexModel : PageModel
+    public class GreetingModel : PageModel
     {
+        private IGreeter _greeter{ get; set; }
+        public string CurrentGreeting { get; set; }
+        public GreetingModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
         public void OnGet()
         {
+            CurrentGreeting = _greeter.greetingsOfTheDay();
         }
     }
 }
