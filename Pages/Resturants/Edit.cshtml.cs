bd7e8abe2a17971ffe9cc12f7da48dd106628962
kishan sharma <kishu02@hotmail.com>
2020-06-05 07:17:43 +0545

    edit resturant razor page

---
+++ Pages/Resturants/Edit.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ode2Food.Services;
+using ode2Food.Models;
+
+namespace ode2Food.Pages.Resturants
+{
+    public class EditModel : PageModel
+    {
+        private SqlResturantData _resturantData;
+        private Resturant resturant;
+
+        public EditModel(SqlResturantData resturantData)
+        {
+            _resturantData = resturantData;
+        }
+        public void OnGet(int id)
+        {
+            resturant = _resturantData.Get(id);
+        }
+    }
+}
