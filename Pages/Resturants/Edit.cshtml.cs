b656fcb21299137455bad557868ad184f1c547dd
kishan sharma <kishu02@hotmail.com>
2020-06-05 08:41:39 +0545

    fixed the issue of unresolved service injection

--- Pages/Resturants/Edit.cshtml.cs
+++ Pages/Resturants/Edit.cshtml.cs
@@ -1,20 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using ode2Food.Services;
 using ode2Food.Models;
+using ode2Food.Services;
 
 namespace ode2Food.Pages.Resturants
 {
     public class EditModel : PageModel
     {
-        private SqlResturantData _resturantData;
+        private IResturant _resturantData;
         public Resturant resturant;
 
-        public EditModel(SqlResturantData resturantData)
+        public EditModel(IResturant resturantData)
         {
             _resturantData = resturantData;
         }
@@ -23,8 +24,18 @@ namespace ode2Food.Pages.Resturants
             resturant = _resturantData.Get(id);
             if (resturant == null)
                 return RedirectToAction("Index", "Home");
-
+            else
                 return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return Page();
         }
     }
 }
